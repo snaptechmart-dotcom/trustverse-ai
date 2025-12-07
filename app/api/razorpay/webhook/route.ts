912d9a4a75b077ecff2bbf0a628eb38121566d05
snaptechmart <spanptechmart@gmail.com>
2025-12-07 20:43:39 +0530

    fix: correct relative path depth

--- app/api/razorpay/webhook/route.ts
+++ app/api/razorpay/webhook/route.ts
@@ -1,9 +1,10 @@
 export const runtime = "nodejs";
 
 import crypto from "crypto";
-import dbConnect from "../../lib/dbConnect";
-import User from "../../models/User";
+import dbConnect from "../../../lib/dbConnect";
+import User from "../../../models/User";
 import { NextResponse } from "next/server";
+
 
 export async function POST(req: Request) {
   await dbConnect();
