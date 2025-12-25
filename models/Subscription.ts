0bcea9b2e810670c4a2eaf05756968155f821ab4
snaptechmart <spanptechmart@gmail.com>
2025-12-25 11:28:07 +0530

    Fix Subscription schema for Razorpay & test-db

--- models/Subscription.ts
+++ models/Subscription.ts
@@ -1,15 +1,46 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, models, model } from "mongoose";
 
-const SubscriptionSchema = new mongoose.Schema(
+const SubscriptionSchema = new Schema(
   {
-    subscriptionId: { type: String, required: true, unique: true },
-    planId: String,
-    status: String,
-    paymentMethod: String,
-    userEmail: String,
+    subscriptionId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    planId: {
+      type: String,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "created",
+        "authenticated",
+        "active",
+        "paused",
+        "cancelled",
+        "completed",
+      ],
+      required: true,
+    },
+
+    paymentMethod: {
+      type: String,
+      default: "upi",
+    },
+
+    currentPeriodEnd: {
+      type: Date,
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Subscription ||
-  mongoose.model("Subscription", SubscriptionSchema);
+export default models.Subscription ||
+  model("Subscription", SubscriptionSchema);
